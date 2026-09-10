d27068aae8d53678bc4ee597f8382eef168a28d2
Kishore Karthick <kishorekarthick183@gmail.com>
2026-09-10 23:27:31 +0530

    Add job1,2 into jobs & iterate to display

--- app.rb
+++ app.rb
@@ -12,5 +12,13 @@ class Job
   end 
 end 
 
-job = Job.new(1, "software engineering intern", 13_000)
-job.display
+jobs = []
+
+job1 = Job.new(1, "software engineering intern", 13_000)
+job2 = Job.new(2, "frontend developer", 10_000)
+jobs << job1
+jobs << job2
+
+jobs.each do |job|
+  job.display
+end
